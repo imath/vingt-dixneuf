f455aaf6a30c89d402aeaf355c7e5818369d8513
imath <math.viet@gmail.com>
2020-03-23 18:38:01 +0100

    Manage BuddyPress page access

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # vingtdixneuf
 
-Un thème enfant de TwentyNineteen.
+Un thème enfant de TwentyNineteen pour illustrer comment mettre en place des zones réservées par type de membre BuddyPress.
+
+## Définir les types de membre BuddyPress
+
+Voir la fonction `vingtdixneuf_register_member_types_with_directory()` du fichier [inc/bp-customs.php](./inc/bp-customs.php)
+
+## Créer des menus WordPress
+
+![menus](./screenshots/menus.png)
+
+### Menu Non connecté
+
+- Créer un menu "Non connecté" et définir son emplacement sur Principal.
+- Ajouter l'accueil et la navigation connexion de la section "BuddyPress
+
+### Menu Participants
+
+- Créer un menu "Participants" sans définir son emplacement.
+- Ajouter l'accueil, un lien personnalisé vers la page des membres de ce type (ex: site.url/membres/**type/participants/**) et la ou les pages de BuddyPress de votre choix ("Discussions" est la page des activités dans mon exemple).
+
+### Menu Staff
+
+- Créer un menu "Staff" sans définir son emplacement.
+- Ajouter l'accueil, un lien personnalisé vers la page des membres de ce type (ex: site.url/membres/**type/staff/**) et la ou les pages de BuddyPress de votre choix (la page des groupes dans mon exemple).
+
+## Adapter le thème enfant
+
+Voir les lignes 33 à 93 du template [site-branding.php](./template-parts/header/site-branding.php)
+
+## Personnaliser le chargement des pages de BuddyPress
+
+Voir la fonction `vingtdixneuf_user_router()` du fichier [inc/bp-customs.php]
--- inc/bp-custom.php
+++ inc/bp-custom.php
@@ -16,7 +16,12 @@ if ( ! defined( 'ABSPATH' ) ) {
  * S'accroche au hook `bp_register_member_types` de BuddyPress pour définir
  * les types de membre disponibles pour la communauté.
  */
-function bbg_register_member_types_with_directory() {
+function vingtdixneuf_register_member_types_with_directory() {
+	/**
+	 * Pour plus d'informations sur les types de membre BuddyPress
+	 *
+	 * @see https://codex.buddypress.org/developer/member-types/
+	 */
 	$member_types = array(
 		'staff' => array(
 			'labels' => array(
@@ -37,4 +42,93 @@ function bbg_register_member_types_with_directory() {
 		bp_register_member_type( $key_type, $args_type );
 	}
 }
-add_action( 'bp_register_member_types', 'bbg_register_member_types_with_directory' );
+add_action( 'bp_register_member_types', 'vingtdixneuf_register_member_types_with_directory' );
+
+/**
+ * Force l'affichage des membres du même type que celui de l'utilisateur connecté
+ * dans le répertoire des membres.
+ */
+function vingtdixneuf_get_members_directory_permalink( $url = '' ) {
+	$current_user_id     = get_current_user_id();
+	$current_member_type = bp_get_member_type( $current_user_id );
+
+	if ( $current_user_id && 'participants' === $current_member_type ) {
+		$url = trailingslashit( $url ) . 'type/participant/';
+	} elseif ( $current_user_id && 'staff' === $current_member_type ) {
+		$url = trailingslashit( $url ) . 'type/staff/';
+	}
+
+	return $url;
+}
+add_filter( 'bp_get_members_directory_permalink', 'vingtdixneuf_get_members_directory_permalink' );
+
+/**
+ * Donne accès à certaine page BuddyPress en fonction du type de membre connecté.
+ */
+function vingtdixneuf_user_router() {
+	$current_component = bp_current_component();
+
+	// Non connecté.
+	if ( $current_component && ! is_user_logged_in() ) {
+		wp_die(
+			'<h1>' . esc_html__( 'Accès restreint.', 'text-domain' ) . '</h1>' .
+			'<p>' . esc_html__( 'Vous devez être authentifié pour afficher cette page', 'text-domain' ) . '</p>',
+			403
+		);
+	}
+
+	$current_user_id     = get_current_user_id();
+	$current_member_type = bp_get_member_type( $current_user_id );
+
+	// Les pages liées aux membres.
+	if ( bp_is_user() || 'members' === $current_component ) {
+		// Les membres d'un type peuvent consulter le repertoire des membres du même type.
+		if ( 'members' === $current_component && $current_member_type && $current_member_type !== bp_get_current_member_type() ) {
+			wp_die(
+				'<h1>' . esc_html__( 'Accès restreint.', 'text-domain' ) . '</h1>' .
+				'<p>' . esc_html__( 'Votre profil ne vous permet pas de consulter cette page', 'text-domain' ) . '</p>',
+				403
+			);
+		}
+
+		// Les membres d'un type peuvent consulter les pages des membres du même type.
+		if ( bp_is_user() && $current_member_type !== bp_get_member_type( bp_displayed_user_id() ) ) {
+			wp_die(
+				'<h1>' . esc_html__( 'Accès restreint.', 'text-domain' ) . '</h1>' .
+				'<p>' . esc_html__( 'Votre profil ne vous permet pas de consulter ce membre.', 'text-domain' ) . '</p>',
+				403
+			);
+		}
+
+		/**
+		 * Pour les autres pages BuddyPress on utilise le menu correspondant au type de membre
+		 * pour restreindre l'accès.
+		 */
+	} elseif ( is_buddypress() && $current_member_type ) {
+		$menu_items = wp_get_nav_menu_items( $current_member_type );
+		$urls       = wp_list_pluck( $menu_items, 'url' );
+
+		$current_url = wp_parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
+		$has_access  = false;
+
+		foreach ( $urls as $url ) {
+			if ( false !== strpos( $url, $current_url ) ) {
+				$has_access = true;
+			}
+		}
+
+		// Cas spécifique des groupes.
+		if ( bp_is_group() && in_array( bp_get_groups_directory_permalink(), $urls, true ) ) {
+			$has_access = true;
+		}
+
+		if ( ! $has_access ) {
+			wp_die(
+				'<h1>' . esc_html__( 'Accès restreint.', 'text-domain' ) . '</h1>' .
+				'<p>' . esc_html__( 'Votre profil ne vous permet pas de consulter cette zone du site.', 'text-domain' ) . '</p>',
+				403
+			);
+		}
+	}
+}
+add_action( 'bp_screens', 'vingtdixneuf_user_router' );
